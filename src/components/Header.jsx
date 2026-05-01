8d5a4bb5b5858e29f187007e97377528933e1ff5
kashfi002 <thekashfi71@gmail.com>
2026-05-01 15:29:04 +0600

    image url updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import headerImg from '../assets/banner-image.jpg';
-
-const Header = () => {
-    console.log(headerImg);
+import Marquee from 'react-fast-marquee';
+const bannerPath = "/assets/banner-image.jpg";
+const getNews = async () => {
+    const res=await fetch("https://moon-lit-library.vercel.app/newarrival.json");
+    const data=await res.json();
+    return data;
+}
+const Header = async () => {
+    const books=await getNews();
     return (
+        <div>
        <div className='container mx-auto my-4'  
             style={{
-                backgroundImage: `url(${headerImg.src})`,
+               backgroundImage: `url(${bannerPath})`,
                 height: "80vh",
                 width: "100%",
                 backgroundSize: "cover",
@@ -20,6 +26,15 @@ const Header = () => {
         >
             <h1 className='text-5xl font-bold text-white mb-4'>Find Your Next Read</h1>
             <button className='btn btn-primary'>Browse Now</button>
+        </div>
+        <div className='container mx-auto flex bg-gray-100 p-2'>
+            <button className='btn bg-blue-300 text-white'>New Arrivals: </button>
+             <Marquee pauseOnHover={true} speed={100}>{
+                books.map((book)=>
+                    <span key={book.id} className="mx-3 font-semibold"> {book.title} </span>
+                )
+                } <span className="mx-6 font-semibold"> Special Discount on Membership!!! </span></Marquee>
+        </div>
         </div>
     );
 };
--- src/components/TopBooks.jsx
+++ src/components/TopBooks.jsx
@@ -1,9 +1,37 @@
+import Image from 'next/image';
 import React from 'react';
-
-const TopBooks = () => {
+const getTopBooks = async () => {
+    const res=await fetch("https://moon-lit-library.vercel.app/data.json");
+    const books=await res.json();
+    const topBooks=books.slice(7,11);
+    return topBooks;
+}
+const TopBooks = async () => {
+const books=await getTopBooks();
     return (
         <div>
-            Top Books
+            <h2>Featured Books</h2>
+            {
+                books.map((book) => 
+                <div key={book.id} className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src={book.image_url}
+      height={200}
+      width={200}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{book.title}</h2>
+    <p>{book.description}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+            )
+            }
+           
         </div>
     );
 };
